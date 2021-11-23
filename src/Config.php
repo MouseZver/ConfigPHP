40e315e62d04572363e097493e71c463304202e0
MouseZver <mouse-zver@xaker.ru>
2021-11-24 02:08:04 +0300

    Full Up

--- src/Config.php
+++ src/Config.php
@@ -10,39 +10,69 @@ declare ( strict_types = 1 );
 
 namespace Nouvu\Config;
 
+use InvalidArgumentException;
+
 final class Config
 {
-	private $return;
+	private mixed $return;
 	
 	public function __construct ( private array $config = [], private string $separator = '.' )
 	{}
 
-	public function set( string $string = null, callable $callable ): void
+	public function set( string | int $offset = null, callable $callable ): void
 	{
-		$this -> segments( $string );
+		$this -> segments( 'set', $offset );
 		
 		$callable( $this -> return );
 	}
 
-	public function get( string $string = null, mixed $default = null ): mixed
+	public function get( string | int $offset = null, mixed $default = null ): mixed
 	{
-		$this -> segments( $string );
-		
-		return $this -> return ??= $default;
+		try
+		{
+			$this -> segments( 'get', $offset );
+			
+			return $this -> return;
+		}
+		catch ( InvalidArgumentException )
+		{
+			return $default;
+		}
 	}
 	
-	private function segments( string | null $string ): void
+	public function has( string | int $offset ): bool
+	{
+		try
+		{
+			$this -> segments( 'has', $offset );
+			
+			return true;
+		}
+		catch ( InvalidArgumentException )
+		{
+			return false;
+		}
+	}
+	
+	private function segments( string $method, string | int | null $offset ): void
 	{
 		$this -> return = &$this -> config;
 		
-		if ( is_null ( $string ) )
+		if ( is_null ( $offset ) )
 		{
 			return;
 		}
 		
-		foreach ( explode ( $this -> separator, $string ) AS $name )
+		foreach ( explode ( $this -> separator, ( string ) $offset ) AS $name )
 		{
-			$this -> return = &$this -> return[$name];
+			if ( $method == 'set' || ( isset ( $this -> return[$name] ) || is_array ( $this -> return ) && array_key_exists ( $name, $this -> return ) ) )
+			{
+				$this -> return = &$this -> return[$name];
+			}
+			else
+			{
+				throw new InvalidArgumentException( 'Not found key name - ' . $name );
+			}
 		}
 	}
 }
